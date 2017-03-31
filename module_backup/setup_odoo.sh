63eaf11cd16393c5d9861e20d03eb6dd155e270d
rotschadel <mikadaba@hotmail.fr>
2017-03-31 13:42:00 +0200

    Check provision scripts for vagrant boxes.

--- module_backup/setup_odoo.sh
+++ module_backup/setup_odoo.sh
@@ -6,13 +6,23 @@ wget https://raw.githubusercontent.com/Yenthe666/InstallScript/10.0/odoo_install
 chmod +x odoo_install.sh
 ./odoo_install.sh
 
-sudo -u postgres psql -c <<PG_CONFIG
+sudo -u postgres psql -c "createuser -sPE vagrant"
+
+<<PG_CONFIG
 CREATE USER vagrant WITH PASSWORD vagrant;
 ALTER USER vagrant WITH SUPERUSER;
 PG_CONFIG
 
+cat >> /home/vagrant/.bashrc <<BASH_CONFIG
+
+config_path=/home/vagrant/SharedFolder/.config_sh
+if [ -x \$config_path ]; then
+    source \$config_path
+fi
+BASH_CONFIG
+
 <<COMMENT
-sudo su -postgres
+sudo su postgres
 createuser -sPE vagrant
 PG_CONFIG
 COMMENT
--- module_backup/setup_rsnapshot.sh
+++ module_backup/setup_rsnapshot.sh
@@ -4,6 +4,10 @@ apt-get update
 apt-get install rsnapshot -y
 
 <<DOC
+To reset ssh-config with odoo machine:
+sudo ssh-keygen -f "/root/.ssh/known_hosts" -R 192.168.50.4
+sudo ssh-copy-id -i /root/.ssh/id_rsa.pub vagrant@192.168.50.4
+
 To fetch remote specific database:
 sudo ssh vagrant@192.168.50.4 pg_dump testBase
 
